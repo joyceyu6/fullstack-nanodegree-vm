fbefb6946dc85eb8bdaa7adf7cb791123ff4aad0
joyce yu <yumengnan@gmail.com>
2019-07-14 16:43:13 -0700

    fix one small indent error

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -80,7 +80,7 @@ def fbconnect():
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?' \
           'grant_type=fb_exchange_token&client_id=%s&client_secret=%s&' \
-          ' fb_exchange_token=%s' \
+          'fb_exchange_token=%s' \
           % (app_id, app_secret, access_token)
 
     h = httplib2.Http()
